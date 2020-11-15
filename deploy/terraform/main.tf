97f4cfd3a75235a1dbf915b1e5bb1618c1b945b8
Sanson <eduardo.sanson@zoop.com.br>
2020-11-15 00:48:08 -0300

    adicionando deployment

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -43,24 +43,57 @@ module "auto-scalling" {
   min_capacity = var.min_capacity
 }
 
-module "lb" {
-  source = "github.com/eduardosanson/terraform-template-modules//modules/ecs-container-service/lb?ref=master"
-  load_balancer_arn = data.terraform_remote_state.cluster.outputs.alb_arn
+module "alb-target-group" {
+  source = "github.com/eduardosanson/terraform-template-modules//modules/lb-target-group?ref=master"
+  load_balancer_arn = data.terraform_remote_state.cluster.outputs.lb_arn
   vpc_id            = data.terraform_remote_state.init-infra.outputs.vpc_id
-  container_name    = var.application_name
+  target_group_name = var.application_name
   lb_port_redirect  = var.application_port
   health_check_path = var.health_check
+}
+
+module "nlb" {
+  source = "github.com/eduardosanson/terraform-template-modules//modules/lb?ref=master"
+  sg_id           = data.terraform_remote_state.init-infra.outputs.sg
+  vpc_subnets     = data.terraform_remote_state.init-infra.outputs.public_subnet
+  lb-type         = "network"
+  lb-name         = var.application_name
+}
+
+module "nlb-target-group" {
+  source = "./modules/lb-target-group"
+  load_balancer_arn = module.nlb.lb_arn
+  vpc_id            = data.terraform_remote_state.init-infra.outputs.vpc_id
+  target_group_name = var.application_name
+  lb_port_redirect  = var.application_port
+  protocol          = "TCP"
 }
 
 module "service" {
   source = "github.com/eduardosanson/terraform-template-modules//modules/ecs-container-service/service?ref=master"
   role_execution_arn      = module.role.ecs-execution-role-arn
   role_service_arn        = module.role.ecs-service-role-arn
-  lb_target_group_arn     = module.lb.lb_target_group_arn
+  lb_target_group_arn     = [module.alb-target-group.lb_target_group_arn]
   ecs_cluster_id          = data.aws_ecs_cluster.ecs_cluster.id
   cluster_name            = data.aws_ecs_cluster.ecs_cluster.cluster_name
   container_name          = var.application_name
   container_image         = var.application_image
   lb_port_redirect        = var.application_port
   task_defination_path    = var.task_definition_path
+}
+
+module "api-gw" {
+  source = "./modules/api-gw"
+  application_name = var.application_name
+  swagger_path = "file/swagger.yml"
+  nlb_domain_name = data.terraform_remote_state.cluster.outputs.lb_dns
+  nlb_listener_port = var.application_port
+  lb_arn = module.nlb.lb_arn
+}
+
+module "sqs" {
+  source = "./modules/sqs"
+  tags = { Challenge = "priority_queue" }
+  task_role = module.role.ecs-service-role-name
+  application_name = var.application_name
 }
---
+++ deploy/terraform/modules/sqs/variable.tf
@@ -0,0 +1,3 @@
+variable "application_name" {type = string}
+variable "tags"             {type = map(string)}
+variable "task_role"        {}
